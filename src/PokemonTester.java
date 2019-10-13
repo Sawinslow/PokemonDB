ae8df6148ff7ba73ffa456db8804d7472333d0ed
Sawinslow <Sawinslow@users.noreply.github.com>
2019-10-13 17:06:53 -0500

    pushing to branch

--- src/PokemonTester.java
+++ src/PokemonTester.java
@@ -5,7 +5,7 @@ import model.Pokemon;
 
 //Use this for ease of use in testing different methods without using a web form
 public class PokemonTester {
-
+// Tester
 	public static void main(String[] args) {
 		Pokemon charmander = new Pokemon("Charmander", "Fire", 5, 25);
 		PokemonHelper ph = new PokemonHelper();
